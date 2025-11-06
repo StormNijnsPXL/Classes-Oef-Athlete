d87f467158b19856654255f64c815472f67a76b9
StormNijnsPXL <12502139@student.pxl.be>
2025-11-06 10:19:27 +0100

    Athlete class

---
+++ OefAthlete/OefAthlete/Models/Athlete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefAthlete.Models
+{
+    internal class Athlete
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Athlete()
+        {
+
+        }
+        public Athlete(string name, int seconds)
+        {
+
+        }
+    }
+}
--- OefAthlete/OefAthlete/Program.cs
+++ OefAthlete/OefAthlete/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace OefAthlete
+﻿using OefAthlete.Models;
+
+namespace OefAthlete
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Naam: ");
+            string nameInput = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out int secondsInput);
+            Athlete newAthlete = new Athlete(nameInput, secondsInput);
+
         }
     }
 }
